41308a675e42ffc30b6d6095291a6b9188340cd7
Valentino Sanchez Raverta <148900241+ValentinoSanchez00@users.noreply.github.com>
2024-10-15 14:39:41 +0200

    Add files via upload

---
+++ modules/vnet/modules/vnet/variables.tf
@@ -0,0 +1,31 @@
+variable "gr_name" {
+  
+}
+
+variable "vnet_name" {
+ 
+}
+
+variable "vnet_address_space" {
+  
+}
+
+variable "location" {
+ 
+  
+}
+
+variable "owner_tag" {
+  
+}
+
+variable "environment_tag" {
+
+  
+}
+variable "vnet_tags" {
+ 
+}
+
+  
+
