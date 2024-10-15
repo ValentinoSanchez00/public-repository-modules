ad09bbd033774f63b3e9635a78555a0a904fe6bd
Valentino Sanchez Raverta <148900241+ValentinoSanchez00@users.noreply.github.com>
2024-10-15 20:53:41 +0200

    Create variables.tf

---
+++ modules/vnet/variables.tf
@@ -0,0 +1,28 @@
+variable "gr_name" {
+  
+}
+
+variable "vnet_name" {
+ 
+}
+
+variable "vnet_address_space" {
+  
+}
+
+variable "location" {
+ 
+  
+}
+
+variable "owner_tag" {
+  
+}
+
+variable "environment_tag" {
+
+  
+}
+variable "vnet_tags" {
+ 
+}
