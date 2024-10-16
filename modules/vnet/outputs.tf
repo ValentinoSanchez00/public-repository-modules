53e850fc2397a80c0f54ff100ca2bbe51d35fcf6
Valentino Sanchez Raverta <148900241+ValentinoSanchez00@users.noreply.github.com>
2024-10-16 09:12:55 +0200

    Update outputs.tf

--- modules/vnet/outputs.tf
+++ modules/vnet/outputs.tf
@@ -1,1 +1,22 @@
 
+output "vnet_name" {
+    value = module.vnet.vnet_name
+}
+output "vnet_address_space" {
+    value = module.vnet.vnet_address_space
+}
+output "vnet_location" {
+    value = module.vnet.location
+}
+output "vnet_rg" {
+    value = module.vnet.gr_name
+}
+output "vnet_tags" {
+    value = module.vnet.vnet_tags
+}
+output "vnet_owner_tag" {
+    value = module.vnet.owner_tag
+}
+output "vnet_environment_tag" {
+    value = module.vnet.environment_tag
+}
